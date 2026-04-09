c98e075de06dbac90fc67312c5ce61bd618c251e
二货爱吃白萝卜 <smith3816@gmail.com>
2026-04-10 00:28:34 +0800

    fix: use native webpack progress plugin (#293)
    * fix: fallback to native progress plugin for webpack 5.106+
    
    * fix: use native webpack progress plugin

--- src/getWebpackConfig.ts
+++ src/getWebpackConfig.ts
@@ -1,7 +1,6 @@
 import { getProjectPath, resolve } from './utils/projectHelper';
 import * as path from 'path';
 import * as webpack from 'webpack';
-import WebpackBar from 'webpackbar';
 import webpackMerge from 'webpack-merge';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
@@ -179,9 +178,8 @@ ${pkg.name} v${pkg.version}
 Copyright 2015-present, Alipay, Inc.
 All rights reserved.
       `),
-      new WebpackBar({
-        name: '🚚  Ant Design Tools',
-        color: '#2f54eb',
+      new webpack.ProgressPlugin({
+        activeModules: true,
       }),
       new CleanUpStatsPlugin(),
     ],
